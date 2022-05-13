de9a6b4d1d21a3abf488017ed7f2f42381ec4770
KhalilurRahmanKhan <khanjahid373@gmail.com>
2022-05-13 22:44:32 +0600

    add about page content

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className='container mt-5'>
-          
+            <p>My goal is to become a full stack we developer.I want to become expert in javascript. And got a remote job from a foreign country.For this , I am learning different web releated technology.</p>
         </div>
     );
 };
